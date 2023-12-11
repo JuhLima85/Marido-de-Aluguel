08c1d1f5bffa308a81e874a337d22506497a7aac
JuhLima85 <limajuh17@gmail.com>
2023-12-11 10:59:27 -0300

    Criação backend

---
+++ backend/src/main/java/com/codedeving/Marido/de/Aluguel/MaridoDeAluguelApplication.java
@@ -0,0 +1,13 @@
+package com.codedeving.Marido.de.Aluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaridoDeAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaridoDeAluguelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codedeving/Marido/de/Aluguel/MaridoDeAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedeving.Marido.de.Aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaridoDeAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

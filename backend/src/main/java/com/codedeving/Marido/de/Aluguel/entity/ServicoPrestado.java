0117c624405f9315ae5fb68d27a710fc08b5f27a
JuhLima85 <limajuh17@gmail.com>
2023-12-11 11:13:16 -0300

    Entidades Cliente e Serviços Prestados

---
+++ backend/src/main/java/com/codedeving/Marido/de/Aluguel/entity/ServicoPrestado.java
@@ -0,0 +1,51 @@
+package com.codedeving.Marido.de.Aluguel.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServicoPrestado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 150)
+	private String descricao;
+
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+
+	@Column
+	private BigDecimal valor;
+
+	@Column
+	private BigDecimal total;
+
+	@Column
+	@JsonFormat(pattern = "dd/MM/YYYY")
+	private LocalDate data;
+
+}

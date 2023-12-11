c64e3494dae9a3b8eda12d2e98f2a3b6fd3956d7
JuhLima85 <limajuh17@gmail.com>
2023-12-11 12:04:40 -0300

    Entidade Usuário e Enum UsuarioRole

---
+++ backend/src/main/java/com/codedeving/Marido/de/Aluguel/entity/Usuario.java
@@ -0,0 +1,87 @@
+package com.codedeving.Marido.de.Aluguel.entity;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.codedeving.Marido.de.Aluguel.enuns.UsuarioRole;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String login;
+	private String password;
+	private UsuarioRole role;
+
+	public Usuario(String login, String password, UsuarioRole role) {
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == UsuarioRole.ADMIN)
+			return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+		else
+			return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
---
+++ backend/src/main/java/com/codedeving/Marido/de/Aluguel/enuns/UsuarioRole.java
@@ -0,0 +1,16 @@
+package com.codedeving.Marido.de.Aluguel.enuns;
+
+public enum UsuarioRole {
+
+	ADMIN("admin"), USER("user");
+
+	private String role;
+
+	UsuarioRole(String role) {
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+}
